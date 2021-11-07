fd377b7ceb8d073ebfb3e97ba5e038f7267b8444
HexV <86024288+hexzzz2008@users.noreply.github.com>
2021-11-07 14:20:13 +0700

    Create c2t.php

---
+++ c2t.php
@@ -0,0 +1,45 @@
+<?php
+
+/** CODE BY HEXV **/
+
+print "Your Cookie Facebokk: ";
+$COOKIE = trim(fgets(STDIN));
+
+$RESULT = GETTOKEN($COOKIE);
+
+if($RESULT)
+{
+  print "Your Token: ". $RESULT;
+} else {
+  die(" Cookie error! ");
+}
+
+
+
+function GETTOKEN($COOKIE) {
+  $URL_BUSS = "https://business.facebook.com/business_locations/?nav_source=flyout_menu";
+  
+  $INIT = curl_init($URL_BUSS);
+  
+  $PROT = array(
+    
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+    CURLOPT_COOKIE    => $COOKIE,
+    CURLOPT_SSL_VERIFYPEER => FALSE
+    
+  );
+  
+  curl_setopt_array($INIT, $PROT);
+  
+  $EXEC = curl_exec($INIT);
+  
+  
+  if (strpos($EXEC,'["EAAG'))
+  {
+  $CUT_TOKEN = explode('["EAAG', $EXEC);
+  $CUT_TOKEN_2 = explode('"', $CUT_TOKEN[1]);
+  $TOKEN = "EAAG" . trim($CUT_TOKEN_2[0]);
+  }
+  return $TOKEN;
+}
